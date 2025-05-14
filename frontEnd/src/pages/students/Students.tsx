06076be420e676f07f0e0468929401652e256096
Your Name <amine.triki15@gmail.com>
2025-05-14 14:17:47 +0100

    edting students

--- frontEnd/src/pages/students/Students.tsx
+++ frontEnd/src/pages/students/Students.tsx
@@ -1,7 +1,81 @@
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(
+  name: string,
+  year_of_birth: number,
+  email: string,
+  city: string,
+) {
+  return { name, year_of_birth, email, city };
+}
+
+const rows = [
+  createData('lee', 1998, 'a@z3cc.cc', "ste",),
+  createData('yasuo', 237, 'b@z3cc.cc', "us" ),
+  createData('jinx', 262, 'c@z3cc.cc', "uk"),
+];
 
 const Students = () => {
   return (
-    <main>students</main>
+    <main>
+      <Container maxWidth="md">
+
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell align="center" >name </StyledTableCell>
+            <StyledTableCell align="center" >year of birth</StyledTableCell>
+            <StyledTableCell align="center">email</StyledTableCell>
+            <StyledTableCell align="center">city</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell align="center" component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="center">{row.year_of_birth}</StyledTableCell>
+              <StyledTableCell align="center">{row.email}</StyledTableCell>
+              <StyledTableCell align="center">{row.city}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+
+          </Container>
+    </main>
   )
 }
 
