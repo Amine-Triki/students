8356b2650385b8238c05a8e8e416b4e82c01b10b
Your Name <amine.triki15@gmail.com>
2025-06-01 21:30:06 +0100

    fix

--- backEnd/src/index.ts
+++ backEnd/src/index.ts
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
@@ -25,7 +25,7 @@ app.use(
   })
 );
 
-app.use("/students", studentsRouter);
+app.use("/api/students", studentsRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
